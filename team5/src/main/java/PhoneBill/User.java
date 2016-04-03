ffc5fd384ad46081f0e6edf2fc5229e82c521bfd
joon3008 <joon4141@gmail.com>
2016-04-03 13:10:47 +0900

    user 작성 3개의 값을 입력받아 출력하는 main함수도 user에 포함시켰습니다.

--- team5/src/main/java/PhoneBill/User.java
+++ team5/src/main/java/PhoneBill/User.java
@@ -1,5 +1,35 @@
 package PhoneBill;
+import java.util.Scanner;
 
 public class User {
+	private String plan;
+	private int numsub;
+	private double usage;
+	
+	User(String type, int subnums, double usages ){
+		this.plan = type;
+		this.numsub = subnums;
+		this.usage = usages;
+	}
+
+	public void printbill(){
+		Bill bill = new Bill(plan, numsub, usage);
+		bill.print();
+	}
+	
+	public static void main(String[] args){
+		String plan;
+		int numsub;
+		double usage;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("플랜을 입력하세요.");
+		plan = scanner.next();
+		System.out.println("연결된 계정수를 입력하세요.");
+		numsub = scanner.nextInt();
+		System.out.println("사용량을 입력하세요");
+		usage = scanner.nextDouble();
+		User user = new User(plan,numsub,usage);
+		user.printbill();
+	}
 
 }
