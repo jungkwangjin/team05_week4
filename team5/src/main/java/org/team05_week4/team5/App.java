87320b8c0d1aeebfc158f973b812b85a89076128
dkfwl <dkfwl@VAIO>
2016-04-03 10:26:41 +0900

    다시 처음생성

---
+++ team5/src/main/java/org/team05_week4/team5/App.java
@@ -0,0 +1,13 @@
+package org.team05_week4.team5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
